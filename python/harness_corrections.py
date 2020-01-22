035f1e787eddc4e5aa2e49b7baaf099d56dde4d7
Fabio Monti <fabio.monti@cern.ch>
2020-01-22 18:04:15 +0100

    add tool to extract the harness corrections, and tool to resubmit only the failed jobs

---
+++ python/harness_corrections.py
@@ -0,0 +1,146 @@
+#!/bin/python
+import os
+import glob
+import math
+from array import array
+import sys
+from optparse import OptionParser
+
+def HarnessLimits(harnessname):
+    
+    folder_args = ROOT.TString(harnessname.replace("/","_")).Tokenize("_");
+    #folder_args.Print();
+    s_ietamin = ROOT.TString()
+    s_ietamax = ROOT.TString()
+    s_iphimin = ROOT.TString()
+    s_iphimax = ROOT.TString()
+    for i in range(0,folder_args.GetEntries()):
+        if folder_args.At(i).String() == "IEta":
+            s_ietamin = folder_args.At(i+1).String()
+            s_ietamax = folder_args.At(i+2).String()
+        elif folder_args.At(i).String() == "IPhi":
+            s_iphimin = folder_args.At(i+1).String()
+            s_iphimax = folder_args.At(i+2).String()
+    print s_ietamin, s_ietamax, s_iphimin, s_iphimax
+    return s_ietamin.Atoi(), s_ietamax.Atoi(), s_iphimin.Atoi(), s_iphimax.Atoi()
+
+def SetAxisTitle(axis,name):
+    if "time" in name:
+        axis.SetTitle("date")
+    elif "lumi" in name:
+        axis.SetTitle("Integrated luminosity (fb^{-1})")
+    elif "median" in name:
+        axis.SetTitle("E/p scale (median)")
+    elif "mean" in name:
+        axis.SetTitle("E/p scale (mean)")
+    elif "template" in name:
+        axis.SetTitle("E/p scale (template fit)")
+
+#parse arguments
+parser = OptionParser()
+parser.add_option('--default_plots',  action='store_true',        dest='default_plots',   default=False,      help='make default plots')
+parser.add_option("-i", "--inputdir", action="store", type="str", dest="inputdir",                    help="input directory")
+parser.add_option("-x", "--xname",    action="store", type="str", dest="xname",    default="0.5*(timemin+timemax)",  help="name in the tree of the x variable")
+parser.add_option("--t_min",    action="store", type="float", dest="t_min",    help="time min")
+parser.add_option("--t_max",    action="store", type="float", dest="t_max",    help="time max")
+parser.add_option("--xlowname",       action="store", type="str", dest="xlowname", default="timemin",  help="name in the tree of the xlow variable")
+parser.add_option("--xupname",        action="store", type="str", dest="xupname",  default="timemax",  help="name in the tree of the xup variable")
+parser.add_option("-y", "--yname",    action="store", type="str", dest="yname",  default="scale_Eop_mean", help="name in the tree of the y variable")
+parser.add_option("--yuncname",           action="store", type="str", dest="yuncname", default="",     help="name in the tree of the y uncertainty variable")
+parser.add_option("-o", "--outdir",    action="store",      type="str", dest="outdir",          default="",       help="output directory")
+
+(options, args) = parser.parse_args()
+
+#create outdir
+if(options.outdir == ""):
+    outdir=options.inputdir
+else:
+    outdir=options.outdir
+    os.system("mkdir -p "+outdir)
+
+import ROOT as ROOT
+
+#build the dictionary of the TChains
+chain_dict = {}
+for dirname in os.listdir(options.inputdir):
+    fullpath = os.path.join(options.inputdir,dirname)
+    print fullpath
+    print os.listdir(fullpath)
+    if not os.path.isdir(fullpath):
+        continue
+    if dirname.find("IEta")!=-1 and dirname.find("IPhi")!=-1:
+        print "creating chain"
+        harnessname = dirname
+        chain_dict[harnessname] = ROOT.TChain(harnessname,harnessname)
+        print str(chain_dict[harnessname].Add(fullpath+"/out_file_*_scalemonitoring.root"))#to be properly modified 
+
+print "%i chains created"%len(chain_dict)
+
+#build the dictionary of the TGraphAsymmErrors
+graph_dict = {}
+for harnessname, chain in chain_dict.items():
+    print harnessname+" - "+str(chain.GetEntries())
+    if chain.GetEntries()<=0: 
+        continue
+    if(options.yuncname!=""):
+        Npoints = chain.Draw( "%s:%s:%s"%(options.yname,options.xname,options.yuncname), 
+                              "timemin>%f && timemax<%f"%(options.t_min,options.t_max),
+                              "goff")
+        y   = chain.GetV1()
+        x   = chain.GetV2()
+        ey  = chain.GetV3()
+        #trick to handle c-pointers AKA PyDoubleBuffer
+        x.SetSize(Npoints)
+        y.SetSize(Npoints)
+        ey.SetSize(Npoints)
+        graph_dict[harnessname] = ROOT.TGraphAsymmErrors(Npoints, 
+                                                         array('d',x), array('d',y), 
+                                                         array('d',(0,)*Npoints), array('d',(0,)*Npoints), 
+                                                         array('d',ey), array('d',ey))
+    else:
+        Npoints = chain.Draw( "%s:%s"%(options.yname,options.xname), 
+                              "timemin>%f && timemax<%f"%(options.t_min,options.t_max),
+                              "goff")
+        y   = chain.GetV1()
+        x   = chain.GetV2()
+        #trick to handle c-pointers AKA PyDoubleBuffer
+        x.SetSize(Npoints)
+        y.SetSize(Npoints)
+        graph_dict[harnessname] = ROOT.TGraphAsymmErrors(Npoints, 
+                                                         array('d',x), array('d',y), 
+                                                         array('d',(0,)*Npoints), array('d',(0,)*Npoints),
+                                                         array('d',(0,)*Npoints), array('d',(0,)*Npoints))
+
+#draw the "scale vs time"-like plots
+os.system("mkdir -p %s/fit/EBm/"%outdir)
+os.system("mkdir -p %s/fit/EBp/"%outdir)
+c = ROOT.TCanvas()
+for harnessname, graph in graph_dict.items():
+    if graph.GetN()==0: continue
+    
+    xmin_graph = ROOT.TMath.MinElement(graph.GetN(),graph.GetX())
+    xmax_graph = ROOT.TMath.MaxElement(graph.GetN(),graph.GetX())
+    ietamin,ietamax,iphimin,iphimax = HarnessLimits(harnessname)
+    graph.SetTitle("PN region: %i #leq i#eta #leq %i, %i #leq i#phi #leq %i"%(ietamin,ietamax,iphimin,iphimax)) 
+    graph.Draw("AP")
+    graph.GetXaxis().SetLimits( xmin_graph-0.07*(xmax_graph-xmin_graph), xmax_graph+0.07*(xmax_graph-xmin_graph))
+    if("time" in options.xname):
+        graph.GetXaxis().SetTimeFormat("%d/%m%F1970-01-01 00:00:00");
+        graph.GetXaxis().SetTimeDisplay(1);
+
+    SetAxisTitle(graph.GetXaxis(),options.xname)
+    SetAxisTitle(graph.GetYaxis(),options.yname)
+    graph.GetXaxis().SetTitleOffset(0.8)
+    graph.GetXaxis().SetLabelSize(0.03)
+
+    if ietamin<0:
+        c.Print("%s/fit/EBm/%s.pdf"%(outdir,harnessname))
+        c.Print("%s/fit/EBm/%s.png"%(outdir,harnessname))
+        c.SaveAs("%s/fit/EBm/%s.root"%(outdir,harnessname))
+        c.SaveAs("%s/fit/EBm/%s.C"%(outdir,harnessname))
+    else: 
+        c.Print("%s/fit/EBp/%s.pdf"%(outdir,harnessname))
+        c.Print("%s/fit/EBp/%s.png"%(outdir,harnessname))
+        c.SaveAs("%s/fit/EBp/%s.root"%(outdir,harnessname))
+        c.SaveAs("%s/fit/EBp/%s.C"%(outdir,harnessname))
+    c.Clear()
